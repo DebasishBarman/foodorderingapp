a15b6bd897e7d8a1f527a2ede973a36d3fc75d16
debasish <debdeva298@gmail.com>
2021-03-17 21:15:56 +0530

    Food ordering app

---
+++ app/src/main/java/Models/trackModel.java
@@ -0,0 +1,55 @@
+package Models;
+
+public class trackModel {
+    String price,quantity,status,address,order_id;
+    public trackModel(){
+
+    }
+    public trackModel(String price, String quantity, String status, String address, String order_id) {
+        this.price = price;
+        this.quantity = quantity;
+        this.status = status;
+        this.address = address;
+        this.order_id = order_id;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
+    }
+}
