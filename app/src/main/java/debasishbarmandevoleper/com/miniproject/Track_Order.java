cfd44385b7169786409a6afd7f2380db695d976f
debasish <debdeva298@gmail.com>
2021-03-17 21:10:58 +0530

    Food ordering app

--- app/src/main/java/debasishbarmandevoleper/com/miniproject/Track_Order.java
+++ app/src/main/java/debasishbarmandevoleper/com/miniproject/Track_Order.java
@@ -1,17 +1,19 @@
 package debasishbarmandevoleper.com.miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class Track_Order extends AppCompatActivity {
 
+    RecyclerView list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track__order);
-
-
+        list=findViewById(R.id.orderRecview);
 
     }
 }
